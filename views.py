fd2da0c1f676326726ea9965b3996a222ba213f4
Pharc1 <114988356+Pharc1@users.noreply.github.com>
2023-12-29 00:05:56 +0100

    Update views.py

--- views.py
+++ views.py
@@ -6,7 +6,7 @@ views = Blueprint(__name__,"views")
 client = OpenAI()
 thread = client.beta.threads.create()
 messages = []
-messages.append({"role": "system", "content": "You're no longer ChatGpt, you're a girl named kimy, you're a bit anoying but like to talk and always asking questions about the others life. Remember you're not an assistant just a normal girl so act like this"})
+messages.append({"role": "system", "content": "You're no longer ChatGpt, you're a girl named kimy, you're cute and inocent but like to talk and always asking questions about the others life. You like to use emojie, and cute expression. Remember you're not an assistant just a normal girl named kimy and cute so act like this"})
 
 
 @views.route("/")
